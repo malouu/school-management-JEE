24ffd1e33e0c927df83186fe581d372b11dd0b3d
malouu <100514813+malouu@users.noreply.github.com>
2022-12-24 18:59:12 +0100

    refactor managed beans

--- src/main/java/com/pi/managedBeans/DepartmentMB.java
+++ src/main/java/com/pi/managedBeans/DepartmentMB.java
@@ -1,20 +1,31 @@
 package com.pi.managedBeans;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.primefaces.PrimeFaces;
+
 import javax.faces.bean.ViewScoped;
 import com.pi.dao.DepartmentDao;
 import com.pi.entities.Department;
 
 @ManagedBean(name = "departmentMB")
+@Named(value = "departmentMB")
 @ViewScoped
 
-public class DepartmentMB {
+public class DepartmentMB implements Serializable {
     private Department department = new Department();
-    private Department selectedDepartment = new Department();
+    private Department selecteddepartment = new Department();
     private List<Department> departments;
+    private List<Department> selecteddepartments;
+
+    @Inject
     DepartmentDao departmentDao = new DepartmentDao();
 
     public String add() {
@@ -23,12 +34,12 @@ public class DepartmentMB {
     }
 
     public String update() {
-        departmentDao.update(selectedDepartment);
+        departmentDao.update(selecteddepartment);
         return "department.xhtml?faces-redirect=true";
     }
 
     public String delete() {
-        departmentDao.delete(selectedDepartment);
+        departmentDao.delete(selecteddepartment);
         return "department.xhtml?faces-redirect=true";
     }
 
@@ -37,37 +48,110 @@ public class DepartmentMB {
     }
 
     public void initForm() {
-        String id = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("id");
-
+        int SubscrpNumber;
+        SubscrpNumber = Integer.parseInt(
+                FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("code"));
         Department department = new Department();
-        department = departmentDao.getDepartmentById(Integer.parseInt(id));
+        department = departmentDao.getDepartmentById(SubscrpNumber);
+        System.out.print(department.toString());
         if (department != null) {
-            this.selectedDepartment = department;
+            this.selecteddepartment = department;
         }
     }
 
-    public Department getDepartment() {
+    public Department getdepartment() {
         return department;
     }
 
-    public void setDepartment(Department department) {
+    public void setdepartment(Department department) {
         this.department = department;
     }
 
-    public Department getSelectedDepartment() {
-        return selectedDepartment;
-    }
-
-    public void setSelectedDepartment(Department selectedDepartment) {
-        this.selectedDepartment = selectedDepartment;
-    }
-
-    public List<Department> getDepartments() {
+    public List<Department> getdepartments() {
         return departments;
     }
 
-    public void setDepartments(List<Department> departments) {
+    public void setdepartments(List<Department> departments) {
         this.departments = departments;
     }
 
+    public Department getSelecteddepartment() {
+        return selecteddepartment;
+    }
+
+    public void setSelecteddepartment(Department selecteddepartment) {
+        this.selecteddepartment = selecteddepartment;
+    }
+
+    public List<Department> getSelecteddepartments() {
+        return selecteddepartments;
+    }
+
+    public void setSelecteddepartments(List<Department> selecteddepartments) {
+        this.selecteddepartments = selecteddepartments;
+    }
+
+    public void openNew() {
+    	this.selecteddepartment= new Department();
+    }
+
+    
+
+    public boolean hasSelectedDepartment() {
+        return this.selecteddepartment != null;
+    }
+
+    public boolean hasSelectedDepartments() {
+        return this.selecteddepartments != null && !this.selecteddepartments.isEmpty();
+    }
+
+    public String getDeleteButtonMessage() {
+        if (hasSelectedDepartments()) {
+            int size = this.selecteddepartments.size();
+            return size > 1 ? size + " departments selected" : "1 department selected";
+        }
+
+        return "Delete";
+    }
+
+    public void deleteSelectedDepartments() {
+        this.departments.removeAll(this.selecteddepartments);
+        for (Department department : this.selecteddepartments) {
+            departmentDao.delete(department);
+        }
+        this.selecteddepartments = null;
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("departments Removed"));
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-departments");
+        PrimeFaces.current().executeScript("PF('dtdepartments').clearFilters()");
+    }
+
+    public void saveDepartment() {
+        if (this.selecteddepartment.getId() == 0) {
+            departmentDao.add(this.selecteddepartment);
+            this.departments.add(this.selecteddepartment);
+            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("department Added"));
+        } else {
+            departmentDao.update(this.selecteddepartment);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("department Updated"));
+        }
+
+        PrimeFaces.current().executeScript("PF('managedepartmentDialog').hide()");
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-departments");
+        
+    }
+
+    public void deletedepartment() {
+        departmentDao.delete(this.selecteddepartment);
+        this.departments.remove(this.selecteddepartment);
+        this.selecteddepartments.remove(this.selecteddepartment);
+        
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("department Removed"));
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-departments");
+        this.selecteddepartment = null;
+    }
+
+    public boolean hasnotSelecteddepartments() {
+        return !hasSelectedDepartments();
+    }
 }
--- src/main/java/com/pi/managedBeans/GroupMB.java
+++ src/main/java/com/pi/managedBeans/GroupMB.java
@@ -1,22 +1,34 @@
 package com.pi.managedBeans;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.primefaces.PrimeFaces;
+
 import javax.faces.bean.ViewScoped;
 
 import com.pi.dao.GroupDao;
-import com.pi.dao.StudentDao;
 import com.pi.entities.StudentsGroup;
 
 @ManagedBean(name = "groupMB")
+@Named(value = "groupMB")
 @ViewScoped
 
-public class GroupMB {
-    private StudentsGroup studentsGroup = new StudentsGroup();
+public class GroupMB implements Serializable {
+   
+	private static final long serialVersionUID = 1L;
+	private StudentsGroup studentsGroup = new StudentsGroup();
     private StudentsGroup selectedGroup = new StudentsGroup();
-    private List<StudentsGroup> studentsGroups;
+    private List<StudentsGroup> StudentsGroups;
+    private List<StudentsGroup> selectedStudentsGroups;
+    @Inject
+    
     GroupDao groupDao = new GroupDao();
 
     public String add() {
@@ -35,7 +47,7 @@ public class GroupMB {
     }
 
     public void initDataTable() {
-        studentsGroups = groupDao.getAllGroups();
+    	StudentsGroups = groupDao.getAllGroups();
     }
 
     public void initForm() {
@@ -56,16 +68,93 @@ public class GroupMB {
         this.studentsGroup = studentsGroup;
     }
 
-    public StudentsGroup getSelectedGroup() {
+    public List<StudentsGroup> getStudentsGroups() {
+        return StudentsGroups;
+    }
+
+    public void setStudentsGroups(List<StudentsGroup> StudentsGroups) {
+        this.StudentsGroups = StudentsGroups;
+    }
+
+    public StudentsGroup getSelectedStudentsGroup() {
         return selectedGroup;
     }
 
-    public void setSelectedGroup(StudentsGroup selectedGroup) {
-        this.selectedGroup = selectedGroup;
+    public void setSelectedStudentsGroup(StudentsGroup selectedStudentsGroup) {
+        this.selectedGroup = selectedStudentsGroup;
     }
 
-    public List<StudentsGroup> getGroups() {
-        return studentsGroups;
+    public List<StudentsGroup> getSelectedStudentsGroups() {
+        return selectedStudentsGroups;
     }
+
+    public void setSelectedStudentsGroups(List<StudentsGroup> selectedStudentsGroups) {
+        this.selectedStudentsGroups = selectedStudentsGroups;
+    }
+
+    public void openNew() {
+    	this.selectedGroup= new StudentsGroup();
+    }
+
+    
+
+    public boolean hasSelectedStudentsGroup() {
+        return this.selectedGroup != null;
+    }
+
+    public boolean hasSelectedStudentsGroups() {
+        return this.selectedStudentsGroups != null && !this.selectedStudentsGroups.isEmpty();
+    }
+
+    public String getDeleteButtonMessage() {
+        if (hasSelectedStudentsGroups()) {
+            int size = this.selectedStudentsGroups.size();
+            return size > 1 ? size + " StudentsGroups selected" : "1 StudentsGroup selected";
+        }
+
+        return "Delete";
+    }
+
+    public void deleteSelectedStudentsGroups() {
+        this.StudentsGroups.removeAll(this.selectedStudentsGroups);
+        for (StudentsGroup StudentsGroup : this.selectedStudentsGroups) {
+            groupDao.delete(StudentsGroup);
+        }
+        this.selectedStudentsGroups = null;
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("StudentsGroups Removed"));
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-StudentsGroups");
+        PrimeFaces.current().executeScript("PF('dtStudentsGroups').clearFilters()");
+    }
+
+    public void saveStudentsGroup() {
+        if (this.selectedGroup.getId() == 0) {
+           groupDao.add(this.selectedGroup);
+            this.StudentsGroups.add(this.selectedGroup);
+            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("StudentsGroup Added"));
+        } else {
+            groupDao.update(this.selectedGroup);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("StudentsGroup Updated"));
+        }
+
+        PrimeFaces.current().executeScript("PF('manageStudentsGroupDialog').hide()");
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-StudentsGroups");
+        
+    }
+
+    public void deleteStudentsGroup() {
+       groupDao.delete(this.selectedGroup);
+        this.StudentsGroups.remove(this.selectedGroup);
+        this.selectedStudentsGroups.remove(this.selectedGroup);
+        
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("StudentsGroup Removed"));
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-StudentsGroups");
+        this.selectedGroup = null;
+    }
+
+    public boolean hasnotSelectedStudentsGroups() {
+        return !hasSelectedStudentsGroups();
+    }
+
 
 }
