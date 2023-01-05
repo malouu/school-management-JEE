060c0326e8108aef7127aa995ebce5cfc649ce91
Khalil Bessaad <bessaadkhalil@gmail.com>
2023-01-05 16:10:14 +0100

    GradeDao and GradeTypeDao

---
+++ src/main/java/com/pi/dao/GradeTypeDao.java
@@ -0,0 +1,43 @@
+package com.pi.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import com.pi.entities.GradeType;
+import com.pi.utils.JPAutil;
+
+public class GradeTypeDao {
+
+    private EntityManager entityManager = JPAutil.getEntityManager("SchoolManagement");
+
+    public void add(GradeType gradeType) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(gradeType);
+        entityManager.getTransaction().commit();
+    }
+
+    public void update(GradeType gradeType) {
+        entityManager.getTransaction().begin();
+        entityManager.merge(gradeType);
+        entityManager.getTransaction().commit();
+    }
+
+    public void delete(GradeType gradeType) {
+        entityManager.getTransaction().begin();
+        gradeType = entityManager.merge(gradeType);
+        entityManager.remove(gradeType);
+        entityManager.getTransaction().commit();
+    }
+
+    // get types by id
+    public GradeType getGradeTypeById(int id) {
+        return entityManager.find(GradeType.class, id);
+    }
+
+    // get all grade types
+    public List<GradeType> getAllGradeTypes() {
+        return entityManager.createQuery("select g from GradeType g").getResultList();
+    }
+
+}
