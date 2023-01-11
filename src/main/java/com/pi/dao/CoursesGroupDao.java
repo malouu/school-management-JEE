5ef308345808d15e6ba0f43ca55ebbd110a9f0b0
malouu <100514813+malouu@users.noreply.github.com>
2023-01-11 09:52:14 +0100

    cousegrrp dao

--- src/main/java/com/pi/dao/CoursesGroupDao.java
+++ src/main/java/com/pi/dao/CoursesGroupDao.java
@@ -1,5 +1,7 @@
 package com.pi.dao;
 
+import java.util.List;
+
 import javax.faces.bean.ApplicationScoped;
 
 import javax.inject.Named;
@@ -11,5 +13,43 @@ import com.pi.utils.JPAutil;
 @Named
 @ApplicationScoped
 public class CoursesGroupDao {
+    private EntityManager entityManager = JPAutil.getEntityManager("SchoolManagement");
+    public void add(CoursesGroup coursesGroup) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(coursesGroup);
+		entityManager.getTransaction().commit();
+	}
+
+	public void update(CoursesGroup coursesGroup) {
+		entityManager.getTransaction().begin();
+		entityManager.merge(coursesGroup);
+		entityManager.getTransaction().commit();
+	}
+
+	public void delete(CoursesGroup coursesGroup) {
+		entityManager.getTransaction().begin();
+		coursesGroup = entityManager.merge(coursesGroup);
+		entityManager.remove(coursesGroup);
+		entityManager.getTransaction().commit();
+	}
+    //get coursegroup by id
+    public CoursesGroup getCoursesGroupById(int id) {
+        return entityManager.find(CoursesGroup.class, id);
+    }
+    //get all coursesgroups
+    public List<CoursesGroup> getAllCoursesGroups() {
+        List<CoursesGroup> coursesGroups = entityManager.createQuery("select c from CoursesGroup c").getResultList();
+        return coursesGroups;
+    }
+    //get coursesgroup by name
+    public List<CoursesGroup> getCoursesGroupByName(String name) {
+        List<CoursesGroup> coursesGroups = entityManager.createQuery("select c from CoursesGroup where c.name like :name")
+                .setParameter("name", "%" + name + "%")
+                .getResultList();
+        return coursesGroups;
+    }
+    
+
+
 
 }
